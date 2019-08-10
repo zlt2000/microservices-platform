b37ef9bb016670d6527fa355b8a9ecf851bbe455
zlt <zltdiablo@163.com>
2019-08-10 23:22:48 +0800

    小优化

--- zlt-business/search-center/search-server/src/main/java/com/central/SearchCenterApp.java
+++ zlt-business/search-center/search-server/src/main/java/com/central/SearchCenterApp.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author zlt
  * @date 2019/5/1
  */
 @EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 @EnableConfigurationProperties(IndexProperties.class)
 public class SearchCenterApp {
--- zlt-web/back-web/src/main/resources/static/module/config.js
+++ zlt-web/back-web/src/main/resources/static/module/config.js
@@ -43,50 +43,6 @@ layui.define(function (exports) {
                 value: JSON.stringify(token)
             });
         },
-        // 导航菜单，最多支持三级，因为还有判断权限，所以渲染左侧菜单很复杂，无法做到递归，你需要更多极请联系我添加，添加可以无限添加，只是无法做到递归
-        // 左部菜单
-        // menus:
-        //     [ {
-        //     name: '系统管理',
-        //     icon: 'layui-icon-set',
-        //     url: 'javascript:;',
-        //     subMenus: [{
-        //         name: '用户管理',
-        //         url: '#!user',  // 这里url不能带斜杠，因为是用递归循环进行关键字注册，带斜杠会被q.js理解为其他注册模式
-        //         path: 'system/user_search.html',
-        //         auth: 'back:user:query'
-        //     }, {
-        //         name: '角色管理',
-        //         url: '#!role',
-        //         path: 'system/role.html',
-        //         auth: 'back:role:query'
-        //     },  {
-        //         name: '菜单管理',
-        //         url: '#!menus',
-        //         path: 'system/menus.html',
-        //         auth: 'back:menu:query'
-        //     }, {
-        //         name: '权限管理',
-        //         url: '#!permissions',
-        //         path: 'system/permissions.html',
-        //         auth: 'back:permission:query'
-        //     }, {
-        //         name: '我的信息',
-        //         url: '#!myInfo',
-        //         path: 'system/myInfo.html',
-        //         hidden :true
-        //     }, {
-        //         name: 'API文档',
-        //         url: '#!api',
-        //         path: 'http://127.0.0.1:9200/swagger-ui.html'
-        //     }, {
-        //         name: '监控中心',
-        //         url: '#!monitor',
-        //         path: 'http://127.0.0.1:9001/#/wallboard'
-        //         //自定义一个字段 携带参数
-        //     }
-        //     ]
-        // }],
         // 当前登录的用户
         getUser: function () {
             var u = layui.data(config.tableName).login_user;
