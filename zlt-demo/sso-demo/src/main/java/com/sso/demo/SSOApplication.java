b4d8fa93b931bc289fe3449f31c760a5b8c55945
zlt <zltdiablo@163.com>
2020-02-23 21:21:15 +0800

    增加sso单点登录demo

---
+++ zlt-demo/sso-demo/src/main/java/com/sso/demo/SSOApplication.java
@@ -0,0 +1,18 @@
+package com.sso.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zlt
+ * @date 2020/2/22
+ * <p>
+ * Blog: https://blog.csdn.net/zlt2000
+ * Github: https://github.com/zlt2000
+ */
+@SpringBootApplication
+public class SSOApplication  {
+    public static void main(String[] args) {
+        SpringApplication.run(SSOApplication.class, args);
+    }
+}
