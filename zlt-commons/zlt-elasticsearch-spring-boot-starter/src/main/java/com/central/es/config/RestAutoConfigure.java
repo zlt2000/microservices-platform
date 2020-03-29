3798d5448fffff7601ca1327bfe28f8aa69d2978
zlt <zltdiablo@163.com>
2020-03-29 16:18:48 +0800

    新增zlt-elasticsearch-spring-boot-starter工程，支持xpack和es7.x版本

---
+++ zlt-commons/zlt-elasticsearch-spring-boot-starter/src/main/java/com/central/es/config/RestAutoConfigure.java
@@ -0,0 +1,75 @@
+package com.central.es.config;
+
+import com.central.es.properties.RestClientPoolProperties;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.elasticsearch.rest.RestClientBuilderCustomizer;
+import org.springframework.boot.autoconfigure.elasticsearch.rest.RestClientProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.PropertyMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+
+/**
+ * es自动配置
+ *
+ * @author zlt
+ * @date 2020/3/28
+ * <p>
+ * Blog: https://blog.csdn.net/zlt2000
+ * Github: https://github.com/zlt2000
+ */
+@EnableConfigurationProperties(RestClientPoolProperties.class)
+public class RestAutoConfigure {
+    @Bean
+    public RestClientBuilderCustomizer restClientBuilderCustomizer(RestClientPoolProperties poolProperties
+            , RestClientProperties restProperties) {
+        return (builder) -> {
+            setRequestConfig(builder, poolProperties);
+
+            setHttpClientConfig(builder, poolProperties, restProperties);
+        };
+    }
+
+    /**
+     * 异步httpclient连接延时配置
+     */
+    private void setRequestConfig(RestClientBuilder builder, RestClientPoolProperties poolProperties){
+        builder.setRequestConfigCallback(requestConfigBuilder -> {
+            requestConfigBuilder.setConnectTimeout(poolProperties.getConnectTimeOut())
+                    .setSocketTimeout(poolProperties.getSocketTimeOut())
+                    .setConnectionRequestTimeout(poolProperties.getConnectionRequestTimeOut());
+            return requestConfigBuilder;
+        });
+    }
+
+    /**
+     * 异步httpclient连接数配置
+     */
+    private void setHttpClientConfig(RestClientBuilder builder, RestClientPoolProperties poolProperties, RestClientProperties restProperties){
+        builder.setHttpClientConfigCallback(httpClientBuilder -> {
+            httpClientBuilder.setMaxConnTotal(poolProperties.getMaxConnectNum())
+                    .setMaxConnPerRoute(poolProperties.getMaxConnectPerRoute());
+
+            PropertyMapper map = PropertyMapper.get().alwaysApplyingWhenNonNull();
+            map.from(restProperties::getUsername).to(username -> {
+                CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+                credentialsProvider.setCredentials(AuthScope.ANY,
+                        new UsernamePasswordCredentials(username, restProperties.getPassword()));
+                httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+            });
+            return httpClientBuilder;
+        });
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ElasticsearchRestTemplate elasticsearchRestTemplate(RestHighLevelClient restHighLevelClient) {
+        return new ElasticsearchRestTemplate(restHighLevelClient);
+    }
+}
---
+++ zlt-commons/zlt-elasticsearch-spring-boot-starter/src/main/java/com/central/es/properties/RestClientPoolProperties.java
@@ -0,0 +1,42 @@
+package com.central.es.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+/**
+ * es的httpClient连接池配置
+ *
+ * @author zlt
+ * @date 2020/3/28
+ * <p>
+ * Blog: https://blog.csdn.net/zlt2000
+ * Github: https://github.com/zlt2000
+ */
+@Setter
+@Getter
+@ConfigurationProperties(prefix = "zlt.elasticsearch.rest-pool")
+@RefreshScope
+public class RestClientPoolProperties {
+    /**
+     * 连接超时时间
+     */
+    private Integer connectTimeOut = 1000;
+    /**
+     * 连接超时时间
+     */
+    private Integer socketTimeOut = 30000;
+    /**
+     * 获取连接的超时时间
+     */
+    private Integer connectionRequestTimeOut = 500;
+    /**
+     * 最大连接数
+     */
+    private Integer maxConnectNum = 30;
+    /**
+     * 最大路由连接数
+     */
+    private Integer maxConnectPerRoute = 10;
+}
