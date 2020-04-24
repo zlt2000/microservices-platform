e00920270bbf4980deb180678b3415ac7f491342
zlt2000 <zltdiablo@163.com>
2020-04-24 12:23:18 +0800

    还原路由配置方式

--- zlt-commons/zlt-ribbon-spring-boot-starter/src/main/java/com/central/common/ribbon/LbIsolationAutoConfigure.java
+++ zlt-commons/zlt-ribbon-spring-boot-starter/src/main/java/com/central/common/ribbon/LbIsolationAutoConfigure.java
@@ -1,15 +1,9 @@
 package com.central.common.ribbon;
 
-import com.alibaba.cloud.nacos.ribbon.NacosServer;
 import com.central.common.constant.ConfigConstants;
-import com.central.common.ribbon.rule.VersionIsolationRule;
-import com.netflix.loadbalancer.IRule;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import com.central.common.ribbon.config.RuleConfigure;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.netflix.ribbon.RibbonClientConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 
 /**
  * 自定义负载均衡配置
@@ -21,12 +15,7 @@ import org.springframework.context.annotation.Bean;
  * Github: https://github.com/zlt2000
  */
 @ConditionalOnProperty(value = ConfigConstants.CONFIG_RIBBON_ISOLATION_ENABLED, havingValue = "true")
-@AutoConfigureBefore(RibbonClientConfiguration.class)
+@RibbonClients(defaultConfiguration = {RuleConfigure.class})
 public class LbIsolationAutoConfigure {
-    @Bean
-    @ConditionalOnClass(NacosServer.class)
-    @ConditionalOnMissingBean
-    public IRule versionIsolationRule() {
-        return new VersionIsolationRule();
-    }
+
 }
