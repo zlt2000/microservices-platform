6000d050f1d6f240e75add6bbbc1f0fc4b08f360
zlt2000 <zltdiablo@163.com>
2020-05-20 13:32:05 +0800

    fix #I1HRK2

--- zlt-gateway/sc-gateway/src/main/java/com/central/gateway/config/CorsConfig.java
+++ zlt-gateway/sc-gateway/src/main/java/com/central/gateway/config/CorsConfig.java
@@ -2,6 +2,8 @@ package com.central.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
@@ -20,6 +22,7 @@ import org.springframework.web.util.pattern.PathPatternParser;
 public class CorsConfig {
     private static final String ALL = "*";
 
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     @Bean
     public CorsWebFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
