206028b348c47b8ca0227974c31f85346de8519f
dengliming <liming.d.pro@gmail.com>
2020-05-24 16:30:17 +0800

    Remove the expression which is always true.

--- zlt-job/job-core/src/main/java/com/xxl/job/core/log/XxlJobFileAppender.java
+++ zlt-job/job-core/src/main/java/com/xxl/job/core/log/XxlJobFileAppender.java
@@ -200,14 +200,12 @@ public class XxlJobFileAppender {
 		BufferedReader reader = null;
 		try {
 			reader = new BufferedReader(new InputStreamReader(new FileInputStream(logFile), "utf-8"));
-			if (reader != null) {
-				StringBuilder sb = new StringBuilder();
-				String line = null;
-				while ((line = reader.readLine()) != null) {
-					sb.append(line).append("\n");
-				}
-				return sb.toString();
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line).append("\n");
 			}
+			return sb.toString();
 		} catch (IOException e) {
 			logger.error(e.getMessage(), e);
 		} finally {
