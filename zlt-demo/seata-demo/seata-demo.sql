5f5a41f6d2baa410d84ed1651f8bd175109586a7
belldog <gratefulness@yeah.net>
2021-03-03 21:34:34 +0800

    修复sql脚本执行报错

--- zlt-demo/seata-demo/seata-demo.sql
+++ zlt-demo/seata-demo/seata-demo.sql
@@ -8,7 +8,7 @@ USE `seata-demo`;
 -- you must to init this sql for you business databese. the seata server not need it.
 -- 此脚本必须初始化在你当前的业务数据库中，用于AT 模式XID记录。与server端无关（注：业务数据库）
 -- 注意此处0.3.0+ 增加唯一索引 ux_undo_log
-drop table `undo_log`;
+DROP TABLE IF EXISTS `undo_log`;
 CREATE TABLE `undo_log` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `branch_id` bigint(20) NOT NULL,
