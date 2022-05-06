7526f18d4a10a41a17a7a8cd6f5f9633c5c07ccb
zhult13 <zhult13@chinaunicom.cn>
2022-05-06 23:22:55 +0800

    适配 mybatis-plus 拦截器新配置方式

--- zlt-commons/zlt-db-spring-boot-starter/src/main/java/com/central/db/config/TenantAutoConfigure.java
+++ zlt-commons/zlt-db-spring-boot-starter/src/main/java/com/central/db/config/TenantAutoConfigure.java
@@ -1,14 +1,11 @@
 package com.central.db.config;
 
-import com.baomidou.mybatisplus.core.parser.ISqlParserFilter;
-import com.baomidou.mybatisplus.core.parser.SqlParserHelper;
-import com.baomidou.mybatisplus.extension.plugins.tenant.TenantHandler;
+import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
 import com.central.common.context.TenantContextHolder;
 import com.central.common.properties.TenantProperties;
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.expression.NullValue;
 import net.sf.jsqlparser.expression.StringValue;
-import org.apache.ibatis.mapping.MappedStatement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -25,13 +22,13 @@ public class TenantAutoConfigure {
     private TenantProperties tenantProperties;
 
     @Bean
-    public TenantHandler tenantHandler() {
-        return new TenantHandler() {
+    public TenantLineHandler tenantLineHandler() {
+        return new TenantLineHandler() {
             /**
              * 获取租户id
              */
             @Override
-            public Expression getTenantId(boolean where) {
+            public Expression getTenantId() {
                 String tenant = TenantContextHolder.getTenant();
                 if (tenant != null) {
                     return new StringValue(TenantContextHolder.getTenant());
@@ -40,36 +37,15 @@ public class TenantAutoConfigure {
             }
 
             /**
-             * 获取租户列名
-             */
-            @Override
-            public String getTenantIdColumn() {
-                return "tenant_id";
-            }
-
-            /**
              * 过滤不需要根据租户隔离的表
              * @param tableName 表名
              */
             @Override
-            public boolean doTableFilter(String tableName) {
+            public boolean ignoreTable(String tableName) {
                 return tenantProperties.getIgnoreTables().stream().anyMatch(
                         (e) -> e.equalsIgnoreCase(tableName)
                 );
             }
-        };
-    }
-
-    /**
-     * 过滤不需要根据租户隔离的MappedStatement
-     */
-    @Bean
-    public ISqlParserFilter sqlParserFilter() {
-        return metaObject -> {
-            MappedStatement ms = SqlParserHelper.getMappedStatement(metaObject);
-            return tenantProperties.getIgnoreSqls().stream().anyMatch(
-                    (e) -> e.equalsIgnoreCase(ms.getId())
-            );
         };
     }
 }
