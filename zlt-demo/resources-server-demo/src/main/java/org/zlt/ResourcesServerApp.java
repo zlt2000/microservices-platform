6e27f055f4f917fcda4f3891fc382010a4fe6d1b
zhult13 <zhult13@chinaunicom.cn>
2022-06-26 16:51:13 +0800

    增加资源服务demo(无网络隔离)

---
+++ zlt-demo/resources-server-demo/src/main/java/org/zlt/ResourcesServerApp.java
@@ -0,0 +1,18 @@
+package org.zlt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zlt
+ * @date 2022/6/25
+ * <p>
+ * Blog: https://zlt2000.gitee.io
+ * Github: https://github.com/zlt2000
+ */
+@SpringBootApplication
+public class ResourcesServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ResourcesServerApp.class, args);
+    }
+}
